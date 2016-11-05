c67e6e1194fb4aa95599558c74827532624c546a
ocherwei <ocherwei@gmail.com>
2016-11-05 20:12:03 +0000

    accidentally remove database.js but its cool now

---
+++ server/database.js
@@ -0,0 +1,55 @@
+var fs = require("fs");
+var file = "text.db";
+
+var exists = fs.existsSync(file);
+
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(file);
+
+db.serialize(function(){
+	
+	if(!exists){
+		//createTables();
+		//populateTables();
+	}
+});
+
+db.close();
+
+var createTables = function()
+{
+	//create tables on first run through
+	db.run("CREATE TABLE Client");
+}
+
+module.exports = {
+	
+	group : {
+		
+		create : function(){
+			/*
+			var stmt = db.prepare("INSERT INTO Client VALUES (?)");	
+			stmt.run("ColumnName #data")
+
+			stmt.finalize();
+			*/
+			
+		},
+		display : function(){
+			/*
+			module.exports.clients.push("Hello");
+			
+			return module.exports.clients;
+			*/
+		},	
+	
+	},	
+
+	client: {
+		refresh : function(){}
+	}
+}
+
+
+
+
