e72616b40e3e01de68f0dc2b4183da10f91e3fb8
Nathan Douglas <nd9600@gmail.com>
2016-11-05 20:24:04 +0000

    made register screen

--- android/groute/app/src/main/java/com/leokomarov/groute/home/HomeController.java
+++ android/groute/app/src/main/java/com/leokomarov/groute/home/HomeController.java
@@ -4,26 +4,21 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 
 import com.bluelinelabs.conductor.RouterTransaction;
 import com.bluelinelabs.conductor.changehandler.FadeChangeHandler;
 import com.leokomarov.groute.R;
 import com.leokomarov.groute.controllers.ButterKnifeController;
-import com.leokomarov.groute.mainscreen.MainScreenController;
+import com.leokomarov.groute.register.RegisterController;
 
-import butterknife.BindView;
 import butterknife.OnClick;
 
 public class HomeController extends ButterKnifeController {
 
-    @BindView(R.id.edittext1)
-    EditText edittext1;
 
-    @OnClick(R.id.submitButton)
+    @OnClick(R.id.registerButton)
     void submitButtonBlicked(){
-        String username = edittext1.getText().toString();
-        getRouter().pushController(RouterTransaction.with(new MainScreenController(username))
+        getRouter().pushController(RouterTransaction.with(new RegisterController())
                 .pushChangeHandler(new FadeChangeHandler())
                 .popChangeHandler(new FadeChangeHandler())
         );
