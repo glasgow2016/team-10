033deb96e6ec0c211cb19ef19f9a099a31e13b6f
Nathan Douglas <nd9600@gmail.com>
2016-11-06 04:56:31 +0000

    added confirm dialogs

--- android/groute/app/src/main/java/com/leokomarov/groute/dashboard/GuardianDashboardController.java
+++ android/groute/app/src/main/java/com/leokomarov/groute/dashboard/GuardianDashboardController.java
@@ -1,6 +1,7 @@
 package com.leokomarov.groute.dashboard;
 
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +9,29 @@ import android.view.ViewGroup;
 import com.leokomarov.groute.R;
 import com.leokomarov.groute.controllers.ButterKnifeController;
 
+import butterknife.OnClick;
+
 public class GuardianDashboardController extends ButterKnifeController {
+
+    @OnClick(R.id.isThisMyChild)
+    void isThisMyChildButtonClicked(){
+        new AlertDialog.Builder(getActivity())
+                .setMessage("Is this your child?")
+                .setCancelable(true)
+                .setPositiveButton("Yes", null)
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    @OnClick(R.id.authoriseChild)
+    void authoriseChildButtonClicked(){
+        new AlertDialog.Builder(getActivity())
+                .setMessage("Do you authorise your child to play this game?")
+                .setCancelable(true)
+                .setPositiveButton("Yes", null)
+                .setNegativeButton("No", null)
+                .show();
+    }
 
     @Override
     protected View inflateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
